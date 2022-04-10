daf44e2225e5f4fab1ec99a575fcf0714e685bfb
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2022-04-10 11:15:15 -0300

    feat: add adjustLangCode

---
+++ src/utils/helpers.ts
@@ -0,0 +1,22 @@
+import * as locale from '@/locale'
+
+export const adjustLangCode = (code: string): string => {
+  const [lang, country] = code.replace('-', '_').split('_')
+  const adjustedCountry = country ? '_' + country.toUpperCase() : ''
+  const adjustedCode = lang.toLowerCase() + adjustedCountry
+  const regExpOnlyLettersAndUnderline = /[A-Za-z_]/g
+
+  if (!regExpOnlyLettersAndUnderline.test(adjustedCode)) {
+    console.log(`${adjustedCode} does not follow the language code pattern, using default language (en)`)
+
+    return 'en'
+  }
+
+  if (!locale[adjustedCode] || adjustedCode === 'lang') {
+    console.log(`${adjustedCode} does not exist, using default language (en)`)
+
+    return 'en'
+  }
+
+  return adjustedCode
+}
